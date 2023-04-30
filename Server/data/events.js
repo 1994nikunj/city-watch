f5dab31204b501604a127a8be4e0aea147a086c2
Aditya singh <iamsinghaditya@gmail.com>
2023-04-30 03:49:53 -0400

    no message

--- Server/data/events.js
+++ Server/data/events.js
@@ -3,9 +3,94 @@ const events = mongoCollections.events;
 const { ObjectId } = require("mongodb");
 const helper = require("../helpers");
 
+const createEvent = async (
+	title,
+	description,
+	images,
+	date,
+	location,
+	time,
+	comments
+) => {
+	const eventData = {
+		title: title,
+		description: description,
+		images: images,
+		date: date,
+		location: location,
+		time: time,
+		comments: comments
+	}
+    
+	const eventCollection = await events();
+	const insertInfo = await eventCollection.insertOne(eventData);
+
+	if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+		throw {
+			'code': 500,
+			'message': 'Error: Could not add the event'
+		}
+	}
+
+	const newId = insertInfo.insertedId.toString();
+	const event = await getEventById(newId);
+	event.id = String(newId);
+
+	return event;
+};
+const getAllEvents = async () => {
+    const eventCollection = await events();
+    const eventList = await eventCollection.find({}).toArray();
+    return eventList;
+  };
+const getEventById = async (id) => {
+	const eventCollection = await events();
+	const event = await eventCollection.findOne({ _id: ObjectId(id) });
+	if (event === null) {
+		throw {
+			'code': 404,
+			'message': `Error: No event found with ID: ${id}`
+		}
+	}
+	event.id = id;
+
+	return event;
+}
+
+const addComment = async (eventId, comment) => {
+    const event = await getEventById(eventId);
+  
+    const newComment = {
+      text: comment.text,
+      user: comment.user,
+      date: new Date().toLocaleDateString(),
+      time: new Date().toLocaleTimeString(),
+    };
+  
+    event.comments.push(newComment);
+  
+    const eventCollection = await events();
+    const updateInfo = await eventCollection.updateOne(
+      { _id: ObjectId(eventId) },
+      { $set: event }
+    );
+  
+    if (!updateInfo.acknowledged || updateInfo.modifiedCount === 0) {
+      throw {
+        code: 500,
+        message: "Error: Could not add the comment",
+      };
+    }
+  
+    return await getEventById(eventId);
+  };
 
 // All routes here
 
 // exports
 module.exports = {
+    createEvent,
+    getEventById,
+    getAllEvents,
+    addComment
 };
--- Server/data/users.js
+++ Server/data/users.js
@@ -3,9 +3,53 @@ const users = mongoCollections.users;
 const { ObjectId } = require("mongodb");
 const helper = require("../helpers");
 
+const createUser = async (
+	firstName,
+	lastName,
+	email,
+	password
+) => {
+	const userData = {
+		firstName:firstName,
+        lastName:lastName,
+        email:email,
+        password:password
+	}
+
+	const userCollection = await users();
+	const insertInfo = await userCollection.insertOne(userData);
+
+	if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+		throw {
+			'code': 500,
+			'message': 'Error: Could not add the movie'
+		}
+	}
+
+	const newId = insertInfo.insertedId.toString();
+	const user = await getUserById(newId);
+	user._id = String(newId);
+
+	return user;
+};
+const getUserById = async (id) => {
+	const userCollection = await users();
+	const user = await userCollection.findOne({ _id: ObjectId(id) });
+	if (user === null) {
+		throw {
+			'code': 404,
+			'message': `Error: No movie found with ID: ${id}`
+		}
+	}
+	user._id = id;
+
+	return user;
+}
 
 // All routes here
 
 // exports
 module.exports = {
+    createUser,
+    getUserById
 };
